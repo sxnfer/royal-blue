705cbdb477c5c97bd635b2eee39ce98b3abb2bbb
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-24 14:59:18 +0100

    ♻️ refactor(transform_lambda): refactor transform lambda utils
    - Refactor transform lambda utils to improve code readability and maintainability
    - 【Refactor】:
      - Rename `get_log_item_df_s3_upload` to `get_log_item_and_upload_df_to_s3`
      - Update `get_log_item_and_upload_df_to_s3` to use `StateLogItem` pydantic model
      - Update `add_log_to_result_and_state` to use `LambdaResult` and `StateLogItem` pydantic models
      - Remove unused imports and functions

--- src/utils/transform_lambda_utils/transform_lambda_utils.py
+++ src/utils/transform_lambda_utils/transform_lambda_utils.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 from datetime import datetime
 from types import FunctionType
 from typing import List
@@ -13,13 +12,13 @@ from src.utils.parquets.create_data_frame_from_parquet import (
 from src.utils.parquets.create_parquet_from_data_frame import (
     create_parquet_from_data_frame,
 )
-from src.utils.pydantic_models import FilesToProcessItem
+from src.utils.pydantic_models import LambdaResult, StateLogItem
 from src.utils.s3.add_file_to_s3_bucket import add_file_to_s3_bucket
 from src.utils.s3.get_file_from_s3_bucket import get_file_from_s3_bucket
 
 
 def get_dataframes_from_files_to_process(
-    s3_client: S3Client, bucket: str, files_to_process: List[FilesToProcessItem]
+    s3_client: S3Client, bucket: str, files_to_process: List[StateLogItem]
 ):
     all_df_to_process = {}
     for file_data in files_to_process:
@@ -37,31 +36,19 @@ def get_dataframes_from_files_to_process(
     return all_df_to_process
 
 
-def initialize_transform_state(state, table_names):
-    initialized_state = deepcopy(state)
-    initialized_state["transform_state"] = {"last_updated": None, "tables": {}}
-
-    for table_name in table_names:
-        initialized_state["transform_state"]["tables"][table_name] = {
-            "last_updated": None,
-            "transform_log": [],
-        }
-    return initialized_state
-
-
 # mutates result and state
 def add_log_to_result_and_state(
-    log: FilesToProcessItem,
-    result,
+    log: StateLogItem,
+    result: LambdaResult,
     state,
     table_name: str,
     last_updated: datetime,
-    processing_timestamp: datetime,
+    operation_timestamp: datetime,
 ):
-    result["files_to_process"].append(log)
+    result.files_to_process.append(log)
     state["transform_state"]["tables"][table_name] = {}
     state["transform_state"]["tables"][table_name]["transformation_timestamp"] = (
-        processing_timestamp
+        operation_timestamp
     )
     state["transform_state"]["tables"][table_name]["transform_log"] = []
     state["transform_state"]["tables"][table_name]["transform_log"].append(log)
@@ -73,19 +60,19 @@ def initialize_dim_date(
     create_dim_date_df_func: FunctionType,
     s3_client: S3Client,
     bucket_name: str,
-    result: dict,
+    result: LambdaResult,
     final_state,
 ):
     new_table_name = "dim_date"
     dim_date_df = create_dim_date_df_func("20221102", "20400101")
     dim_date_transformation_time_stamp = datetime.now()
-    dim_date_log_item = get_log_item_df_s3_upload(
+    dim_date_log_item = get_log_item_and_upload_df_to_s3(
         s3_client=s3_client,
         bucket_name=bucket_name,
         last_updated=dim_date_transformation_time_stamp,
         new_table_name=new_table_name,
         df=dim_date_df,
-        transformation_timestamp=dim_date_transformation_time_stamp,
+        operation_timestamp=dim_date_transformation_time_stamp,
         create_parquet_from_df_func=create_parquet_from_data_frame,  # type: ignore
     )
 
@@ -94,22 +81,22 @@ def initialize_dim_date(
         result=result,
         state=final_state,
         last_updated=dim_date_transformation_time_stamp,
-        processing_timestamp=dim_date_transformation_time_stamp,
+        operation_timestamp=dim_date_transformation_time_stamp,
         table_name=new_table_name,
     )
 
 
-def get_log_item_df_s3_upload(
+def get_log_item_and_upload_df_to_s3(
     s3_client: S3Client,
     bucket_name: str,
     last_updated: datetime,
     new_table_name: str,
     df: pd.DataFrame,
-    transformation_timestamp: datetime,
+    operation_timestamp: datetime,
     create_parquet_from_df_func: FunctionType,
 ):
     parquet = create_parquet_from_df_func(df)
-    filename, key = create_parquet_metadata(
+    file_name, key = create_parquet_metadata(
         last_updated,
         new_table_name,
     )
@@ -119,12 +106,12 @@ def get_log_item_df_s3_upload(
     if response.get("error"):
         raise Exception("something went wrong while upload")
 
-    log_item = {
-        "table_name": new_table_name,
-        "key": key,
-        "filename": filename,
-        "last_updated": last_updated,
-        "transformation_timestamp": transformation_timestamp,
-    }
+    log_item = StateLogItem(
+        table_name=new_table_name,
+        last_updated=last_updated,
+        operation_timestamp=operation_timestamp,
+        file_name=file_name,
+        key=key,
+    )
 
     return log_item
