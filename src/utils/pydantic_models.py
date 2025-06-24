311967ec9ccbef32a3d03d2865b6c2a305574b1c
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-24 14:58:26 +0100

    ✨ feat(pydantic_models): implement state management models - introduce pydantic models for managing state in extract, transform and load processes - add models for state logging, table tracking and state items - define models for totesys and warehouse schemas - add data frame model for data processing

--- src/utils/pydantic_models.py
+++ src/utils/pydantic_models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from typing import List
+from typing import Dict, List
 
-from pydantic import BaseModel, TypeAdapter
+import pandas as pd
+from pydantic import BaseModel, ConfigDict, TypeAdapter
 
 
 class ConnectionInfo(BaseModel):
@@ -32,26 +33,85 @@ class EnvironmentVariables(BaseModel):
     environment_variable: str
 
 
-class FilesToProcessItem(BaseModel):
+class StateLogItem(BaseModel):
     table_name: str
-    extraction_timestamp: datetime
     last_updated: datetime
+    operation_timestamp: datetime
     file_name: str
     key: str
 
 
-FilesToProcessList = TypeAdapter(List[FilesToProcessItem])
+class StateTableItem(BaseModel):
+    table_name: str
+    last_updated: datetime | None = None
+    log_items: List[StateLogItem] = []
 
 
-class LambdaResult(BaseModel):
-    files_to_process: List[FilesToProcessItem]
+class StateTablesTotesys(BaseModel):
+    counterparty: StateTableItem = StateTableItem(table_name="counterparty")
+    address: StateTableItem = StateTableItem(table_name="address")
+    staff: StateTableItem = StateTableItem(table_name="staff")
+    design: StateTableItem = StateTableItem(table_name="design")
+    sales_order: StateTableItem = StateTableItem(table_name="sales_order")
+    currency: StateTableItem = StateTableItem(table_name="currency")
+    department: StateTableItem = StateTableItem(table_name="department")
+    purchase_order: StateTableItem = StateTableItem(table_name="purchase_order")
+    payment_type: StateTableItem = StateTableItem(table_name="payment_type")
+    payment: StateTableItem = StateTableItem(table_name="payment")
+    transaction: StateTableItem = StateTableItem(table_name="transaction")
 
 
-class ProcessState(BaseModel):
-    last_updated: datetime | None
-    tables: dict
+class StateTablesWarehouse(BaseModel):
+    dim_counterparty: StateTableItem = StateTableItem(table_name="dim_counterparty")
+    dim_location: StateTableItem = StateTableItem(table_name="dim_location")
+    dim_staff: StateTableItem = StateTableItem(table_name="dim_staff")
+    dim_design: StateTableItem = StateTableItem(table_name="dim_design")
+    fact_sales_order: StateTableItem = StateTableItem(table_name="fact_sales_order")
+    dim_currency: StateTableItem = StateTableItem(table_name="dim_currency")
+    dim_date: StateTableItem = StateTableItem(table_name="dim_date")
+    dim_payment_type: StateTableItem = StateTableItem(table_name="dim_payment_type")
+    dim_transaction: StateTableItem = StateTableItem(table_name="dim_transaction")
+    fact_payment: StateTableItem = StateTableItem(table_name="fact_payment")
+    fact_purchase_order: StateTableItem = StateTableItem(
+        table_name="fact_purchase_order"
+    )
+
+
+class StateItemBase(BaseModel):
+    last_updated: datetime | None = None
+
+
+class StateItemExtract(StateItemBase):
+    tables: StateTablesTotesys = StateTablesTotesys()
+
+
+class StateItemTransform(StateItemBase):
+    tables: StateTablesWarehouse = StateTablesWarehouse()
+
+
+class StateItemLoad(StateItemBase):
+    tables: StateTablesWarehouse = StateTablesWarehouse()
+
+
+class StateItem(BaseModel):
+    last_updated: datetime | None = None
+    tables: Dict[str, StateTableItem] = {}
 
 
 class State(BaseModel):
-    ingest_state: dict
-    transform_state: ProcessState
+    extract_state: StateItem = StateItem()
+    transform_state: StateItem = StateItem()
+    load_state: StateItem = StateItem()
+
+
+class LambdaResult(BaseModel):
+    files_to_process: List[StateLogItem] = []
+
+
+class FactOrDimToProcess(BaseModel):
+    table_name: str
+    data_frame: pd.DataFrame
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+FilesToProcessList = TypeAdapter(List[StateLogItem])
